078e7645968d77e2f3d767bd6616d21134b55a22
Vladyslav Tehlivets <vladyslav.tehlivets@gmail.com>
2019-07-07 22:45:41 +0200

    Corrected controller

--- src/main/kotlin/umcs/testcraftmanshipt/cinemacity/Controller.kt
+++ src/main/kotlin/umcs/testcraftmanshipt/cinemacity/Controller.kt
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TestForController(val returnValue: String = "Hello World") {
+class HomeController(val returnValue: String = "CinemaCity") {
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     fun returnHelloWorldResponse(): ResponseEntity<String> {
         return ResponseEntity.ok(returnValue)
     }
